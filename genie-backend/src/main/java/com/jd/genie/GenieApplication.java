8d06d9c6363b59bbc74fbd2ab2266207ef85b75c
Kross <2813402@qq.com>
2025-07-24 21:32:29 +0800

    1111

--- genie-backend/src/main/java/com/jd/genie/GenieApplication.java
+++ genie-backend/src/main/java/com/jd/genie/GenieApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GenieApplication {
+    //111
     public static void main(String[] args) {
         SpringApplication.run(GenieApplication.class, args);
     }
